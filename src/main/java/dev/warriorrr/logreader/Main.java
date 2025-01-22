01ee59618001e88051d96380b9ace18addd5375c
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-01-22 19:58:32 +0100

    Remove clearing screen on start

--- src/main/java/dev/warriorrr/logreader/Main.java
+++ src/main/java/dev/warriorrr/logreader/Main.java
@@ -3,8 +3,6 @@ package dev.warriorrr.logreader;
 import dev.warriorrr.logreader.console.LogReaderConsole;
 import picocli.CommandLine;
 
-import java.util.Locale;
-
 public class Main {
     public static void main(String[] args) {
         final LogReaderOptions options = new LogReaderOptions();
@@ -13,7 +11,6 @@ public class Main {
 
         final LogReader reader = new LogReader(options);
 
-        clearScreen();
         System.out.println("|##########################################################|");
         System.out.println("|                     LogReader v%version%                     |".replace("%version%", getVersion()));
         System.out.println("|##########################################################|");
@@ -22,15 +19,6 @@ public class Main {
         System.out.println("Logs folder location: " + reader.logsPath().toAbsolutePath());
 
         new LogReaderConsole(reader).readCommands();
-    }
-
-    private static void clearScreen() {
-        try {
-            if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows"))
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            else
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-        } catch (Exception ignored) {}
     }
 
     private static String getVersion() {
