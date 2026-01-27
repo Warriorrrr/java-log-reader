03747fd9ad1b6ff897b01b6350d38f074f61f089
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-01-27 20:06:40 +0100

    Improve feedback and date parsing of date command

--- src/main/java/dev/warriorrr/logreader/commands/DateCommand.java
+++ src/main/java/dev/warriorrr/logreader/commands/DateCommand.java
@@ -3,10 +3,23 @@ package dev.warriorrr.logreader.commands;
 import dev.warriorrr.logreader.LogReader;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
 public class DateCommand extends Command {
+    private final List<String> dateFormatStrings = List.of(
+        "dd-MM-yyyy",
+        "dd/MM/yyyy",
+        "yyyy-MM-dd",
+        "dd MM yyyy"
+    );
+
+    private final List<DateTimeFormatter> dateFormats = dateFormatStrings.stream().map(DateTimeFormatter::ofPattern).toList();
+
     public DateCommand(LogReader reader) {
         super(reader);
     }
@@ -14,24 +27,56 @@ public class DateCommand extends Command {
     @Override
     public void dispatch(String[] args) {
         if (args.length < 1) {
-            System.out.println("Not enough arguments! Usage: date <min|max> [<date>]");
+            System.err.println("Not enough arguments! Usage: date <min|max> [<date>]");
             return;
         }
 
         final String arg = args[0].toLowerCase(Locale.ROOT);
         if (!arg.equals("min") && !arg.equals("max")) {
-            System.out.println("Invalid argument, usage: date <min|max> <date>");
+            System.err.println("Invalid argument, usage: date <min|max> [<date>]");
             return;
         }
 
-        final LocalDate date = args.length > 1 ? LocalDate.parse(args[1]) : null;
-        if (arg.equals("min")) {
+        final boolean minDate = "min".equals(arg);
+
+        if (args.length < 2) {
+            // clear date
+            if (minDate) {
+                reader.setMinDate(null);
+            } else {
+                reader.setMaxDate(null);
+            }
+
+            System.out.println("Cleared " + (minDate ? "minimum" : "maximum") + " date.");
+            return;
+        }
+
+        final List<String> argsList = new ArrayList<>(List.of(args));
+        argsList.removeFirst();
+
+        final LocalDate date = parseDate(String.join(" ", argsList));
+        if (date == null) {
+            System.err.println("Invalid date format specified, format must be one of [" + String.join(", ", this.dateFormatStrings) + "]");
+            return;
+        }
+
+        if (minDate) {
             reader.setMinDate(date);
             System.out.println("Minimum date set to " + date);
         } else {
             reader.setMaxDate(date);
             System.out.println("Maximum date set to " + date);
         }
+    }
+
+    private LocalDate parseDate(final String string) {
+        for (final DateTimeFormatter format : this.dateFormats) {
+            try {
+                return LocalDate.parse(string, format);
+            } catch (DateTimeParseException ignored) {}
+        }
+
+        return null;
     }
 
     @Override
